68b9896a6bebdcd9868488b72aa5949ff3c49a1d
Tiffany Dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2022-02-08 01:33:11 +0100

    [Mercurial_View] début interface projet (quasi rien mais début MercurialView selon maquettes

---
+++ Front DAM Project 2022/View/Mercurial/MercurialView.swift
@@ -0,0 +1,43 @@
+//
+//  MercurialView.swift
+//  Front DAM Project 2022
+//
+//  Created by m1 on 08/02/2022.
+//
+
+import SwiftUI
+
+struct MercurialView: View {
+    var cols = [GridItem(.flexible()), GridItem(.fixed(150))]
+    var body: some View {
+        VStack {
+            VStack {
+                
+            }
+            LazyVGrid(columns: cols, alignment: .leading){
+                Text("Title 1")
+                    HStack {
+                    Button("Modifier", action: {})
+                    Button("delete", action: {})
+                }
+                Text("Title 1")
+                HStack {
+                    Button("Modifier", action: {})
+                    Button("delete", action: {})
+                }
+                Text("Title 1")
+                    HStack {
+                    Button("Modifier", action: {})
+                    Button("delete", action: {})
+                }
+
+            }
+        }
+    }
+}
+
+struct MercurialView_Previews: PreviewProvider {
+    static var previews: some View {
+        MercurialView()
+    }
+}
